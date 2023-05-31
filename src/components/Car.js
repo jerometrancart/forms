d1d84e5f611e95019e4bb9d0cc3c1ed03cc73ec7
jerometrancart <jerome.trancart@gmail.com>
2023-05-31 23:46:57 +0200

    Form dealt with in react

---
+++ src/components/Car.js
@@ -0,0 +1,7 @@
+import car from "../car.png";
+
+const Car = () => {
+    return (<img src={car} alt="Une voiture" />)
+}
+
+export default Car;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,64 @@
 import { Component } from 'react';
-
+import Car from './Car';
 class Form extends Component {
+    state = {
+        username: '',
+        color: '',
+        colors: ["", "red", "green", "blue", "orange"],
+        comment: ""
+    }
+
+    handlePseudo = e => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    handleColor = e => {
+        this.setState({
+            color: e.target.value
+        })
+    }
+
+    handleComments = e => {
+        this.setState({
+            comment: e.target.value
+        })
+    }
+
+    handleSubmitForm = e => {
+        e.preventDefault();
+        console.log(`Username: ${this.state.username}, Color: ${this.state.color}, Comment: ${this.state.comment}`)
+    }
+
     render() {
+        
         return(
             <div>
-                Image ici
+                <Car />
                 <h1>Commentaire</h1>
-                Formulaire
+
+                <form onSubmit={this.handleSubmitForm}>
+                    <div>
+                        <label>Pseudo</label>
+                        <input type="text" value={this.state.username} onChange={this.handlePseudo}/>
+                    </div>
+                    <div>
+                        <label>Couleur</label>
+                        <select onChange={this.handleColor}>
+                            {
+                                this.state.colors.map((color, index) => {
+                                    return <option key={index} value={color}>{color}</option>
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div>
+                        <label>Commentaire</label>
+                        <textarea value={this.state.comment} onChange={this.handleComments}/>
+                    </div>
+                    <button type="submit">Valider</button>
+                </form>
             </div>
         )
     }
