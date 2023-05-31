7f7136ed0942b39bea98c722014741a055b54f87
jerometrancart <jerome.trancart@gmail.com>
2023-05-31 13:26:14 +0200

    App created

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Form extends Component {
+    render() {
+        return(
+            <div>
+                Image ici
+                <h1>Commentaire</h1>
+                Formulaire
+            </div>
+        )
+    }
+}
+
+export default Form;
