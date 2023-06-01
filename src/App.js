823a7919060a8c0dbde801aa82c820f9e44e45eb
jerometrancart <jerome.trancart@gmail.com>
2023-06-01 09:51:13 +0200

    Practice inline css and module css in react

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       
-        <Form />
+        <Form head={true}/>
       
     </div>
   );
---
+++ src/components/Headlight.js
@@ -0,0 +1,10 @@
+const Headlight = () => {
+    return (
+    <div>    
+        <h1 className='blue'>Commentaire 2</h1>
+        <p className='red'>Je suis rouge ou bleu.</p>
+    </div>
+    )
+}
+
+export default Headlight;
